09a0b9bdf3d309fcd1be0be56ebbf376486a5198
fenrhil <fenrhil@gmail.com>
2011-10-29 15:36:34 +0100

    Fixed "syscall0" issue.

--- test/helloworld.h
+++ test/helloworld.h
@@ -1,5 +1,16 @@
+/*
 #include <sys/syscall.h>
 #include <linux/unistd.h>
 
-_syscall0(int, helloworld);
+ * This macro doesn't work:
+ * _syscall0(int, helloworld);
+ */
+
+int helloworld ( void )
+{
+	__asm__ (
+		"movl	$337,%eax\n\t"
+		"int	$0x80\n\t"
+		);
+}
 
